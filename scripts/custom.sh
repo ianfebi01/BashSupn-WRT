eb4c95891b5b5b1c99773833c43c0018f4e689b4
Adi Putra <73918462+Putra-0@users.noreply.github.com>
2023-02-03 11:04:06 +0700

    Create custom.sh

---
+++ scripts/custom.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=================================================
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# Add official OpenClash source
+git clone --depth=1 -b dev https://github.com/vernesong/OpenClash
+
+# HelmiWrt packages
+git clone --depth=1 https://github.com/helmiau/helmiwrt-packages
+
+# Add the default password for the 'root' user（Change the empty password to 'password'）
+sed -i 's/root::0:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' package/base-files/files/etc/shadow
+
+# Add date version
+export DATE_VERSION=$(date -d "$(rdate -n -4 -p pool.ntp.org)" +'%Y-%m-%d')
+sed -i "s/%C/%C (${DATE_VERSION})/g" package/base-files/files/etc/openwrt_release
+
+# Fix mt76 wireless driver
+pushd package/kernel/mt76
+sed -i '/mt7662u_rom_patch.bin/a\\techo mt76-usb disable_usb_sg=1 > $\(1\)\/etc\/modules.d\/mt76-usb' Makefile
+popd
+
+# Rename hostname to OpenWrt
+pushd package/base-files/files/bin
+sed -i 's/ImmortalWrt/BashSupn-Wrt/g' config_generate
+popd
+
+# Change default shell to zsh
+sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
